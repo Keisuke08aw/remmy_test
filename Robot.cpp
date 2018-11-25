0dc4c7ea69a34d3766d676601aa9bd928539de6c
Unknown <julius08aw@gmail.com>
2018-11-26 01:16:14 +0900

    delete comment

--- Robot.cpp
+++ Robot.cpp
@@ -115,7 +115,7 @@ std::vector<float> Robot::direct_kinematics(Eigen::MatrixXd vec_joint_angle)
 //ある手先座標(x,y,z)を入れて、次の関節の角度(θ1, θ2, θ3, θ4)を返す関数
 Eigen::MatrixXd Robot::inverse_kinematics(std::vector<float> target_vec)
 {
-    printf("AAAAAAAAAAAAAAAAAAAA\r\n");
+    // printf("AAAAAAAAAAAAAAAAAAAA\r\n");
     Robot robot;
 
     Eigen::MatrixXd qi_1(3, 1);
@@ -135,10 +135,10 @@ Eigen::MatrixXd Robot::inverse_kinematics(std::vector<float> target_vec)
     float eng_effecotr_z = value_matrix(2);
 
     float delta_x = sqrt(pow((target_x - eng_effecotr_x), 2));
-    printf("target%f\r\n", target_x);
+    // printf("target%f\r\n", target_x);
 
-    printf("END%f\r\n", eng_effecotr_x);
-    printf("DELTA%f\r\n", delta_x);
+    // printf("END%f\r\n", eng_effecotr_x);
+    // printf("DELTA%f\r\n", delta_x);
 
     float delta_y = sqrt(pow((target_y - eng_effecotr_y), 2));
     float delta_z = sqrt(pow((target_z - eng_effecotr_z), 2));
@@ -147,11 +147,11 @@ Eigen::MatrixXd Robot::inverse_kinematics(std::vector<float> target_vec)
     float rad2 = robot.get_Joint2Angle();
     float rad3 = robot.get_Joint3Angle();
 
-    printf("rad1 %f\r\n", rad1);
-    printf("rad2 %f\r\n", rad2);
-    printf("rad3 %f\r\n", rad3);
+    // printf("rad1 %f\r\n", rad1);
+    // printf("rad2 %f\r\n", rad2);
+    // printf("rad3 %f\r\n", rad3);
 
-    printf("BBBBBBBBBBBBBBBBBB\r\n");
+    // printf("BBBBBBBBBBBBBBBBBB\r\n");
 
     Jacobian(0, 0) = -sin(rad1) * cos(rad2) * (5 * cos(rad3) + 5) + (-sin(rad1)) * (5 * sin(rad2) * sin(rad3) + 10);
     Jacobian(0, 1) = cos(rad1) * (-sin(rad2)) * (5 * cos(rad3) + 5) + 0;
@@ -165,9 +165,9 @@ Eigen::MatrixXd Robot::inverse_kinematics(std::vector<float> target_vec)
     Jacobian(2, 1) = -5 * cos(rad2) * (cos(rad3) + 1) - 5 * cos(rad2) * sin(rad3);
     Jacobian(2, 2) = -5 * sin(rad2) * (-sin(rad3)) + 5 * cos(rad2) * cos(rad3);
 
-    printf("Jacobian\r\n");
+    // printf("Jacobian\r\n");
 
-    std::cout << Jacobian << std::endl;
+    // std::cout << Jacobian << std::endl;
     // Eigen::MatrixXd transposed_Jacobian = Jacobian.transpose();
     // std::cout << transposed_Jacobian << std::endl;
     // Eigen::MatrixXd JJ = (Jacobian * transposed_Jacobian);
@@ -178,51 +178,51 @@ Eigen::MatrixXd Robot::inverse_kinematics(std::vector<float> target_vec)
     // std::cout << inv_JJ << std::endl;
     // std::cout << pseudo_Jacobian << std::endl;
 
-    printf("CCCCCCCCCCCCCCCCCCCCC\r\n\r\n");
+    // printf("CCCCCCCCCCCCCCCCCCCCC\r\n\r\n");
 
     //逆ヤコビアン
-    printf("inv_Jacobian\r\n");
+    // printf("inv_Jacobian\r\n");
     inv_Jacobian = Jacobian.inverse();
-    std::cout << inv_Jacobian << std::endl;
+    // std::cout << inv_Jacobian << std::endl;
 
-    printf("DDDDDDDDDDDDDDDDDD\r\n");
+    // printf("DDDDDDDDDDDDDDDDDD\r\n");
 
     //今の各関節のjointのangleをget
     qi << rad1, rad2, rad3;
 
-    printf("EEEEEEEEEEEEEEEEEEEEE\r\n");
+    // printf("EEEEEEEEEEEEEEEEEEEEE\r\n");
 
     //手先が目標に対してどれだけ離れているか計算
     ri << delta_x, delta_y, delta_z;
 
-    printf("FFFFFFFFFFFFFFFFFFF\r\n");
+    // printf("FFFFFFFFFFFFFFFFFFF\r\n");
 
     //学習率
     learning_rate << 0.1, 0, 0,
                     0, 0.1, 0,
                 0, 0, 0.1;
 
-    printf("GGGGGGGGGGGGGGGGGGGGG\r\n");
+    // printf("GGGGGGGGGGGGGGGGGGGGG\r\n");
 
-    std::cout << qi << std::endl;
-    std::cout << learning_rate << std::endl;
-    std::cout << inv_Jacobian << std::endl;
-    std::cout << ri << std::endl;
+    // std::cout << qi << std::endl;
+    // std::cout << learning_rate << std::endl;
+    // std::cout << inv_Jacobian << std::endl;
+    // std::cout << ri << std::endl;
 
-    printf("HHHHHHHHHHHHHHHHHH\r\n");
+    // printf("HHHHHHHHHHHHHHHHHH\r\n");
 
     qi_1 = qi + learning_rate * inv_Jacobian * ri;
     // Eigen::MatrixXd v1 = learning_rate * inv_Jacobian;
     // Eigen::MatrixXd v2 = v1 * ri;
-    printf("HHHHHHHHHHHHHHHHHH\r\n");
+    // printf("HHHHHHHHHHHHHHHHHH\r\n");
 
     // qi_1 = qi + v2;
 
-    printf("IIIIIIIIIIIIIIIIIIII\r\n");
+    // printf("IIIIIIIIIIIIIIIIIIII\r\n");
 
-    std::cout << qi_1 << std::endl;
+    // std::cout << qi_1 << std::endl;
 
-    printf("JJJJJJJJJJJJJJJJJJJ\r\n");
+    // printf("JJJJJJJJJJJJJJJJJJJ\r\n");
 
     return qi_1;
 }
